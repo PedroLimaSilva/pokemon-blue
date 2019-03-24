bda282b76a621afd0bf58abf09d16a8d9db47102
Pedro Lima e Silva <pedrocaslsilva403@gmail.com>
2019-03-24 17:55:38 +0000

    Formatting

--- src/state/actions.js
+++ src/state/actions.js
@@ -6,31 +6,30 @@ export const MOVE_UP = 'MOVE_UP';
 export const MOVE_DOWN = 'MOVE_DOWN';
 export const MOVE_LEFT = 'MOVE_LEFT';
 export const MOVE_RIGHT = 'MOVE_RIGHT';
+export const IDLE = 'IDLE';
 
 export const INTERACT = 'INTERACT';
-
 
 /*
  * action creators
  */
 
-export function move(direction, text) {
-    switch (direction.toUpperCase()) {
-        case 'UP':
-            return { type: MOVE_UP, text };
-        case 'DOWN':
-            return { type: MOVE_DOWN, text };
-        case 'LEFT':
-            return { type: MOVE_LEFT, text };
-        case 'RIGHT':
-            return { type: MOVE_RIGHT, text };
-        default: {
-            return { type: 'IDLE', text }
-        }
+export function move(direction) {
+  switch (direction.toUpperCase()) {
+    case 'UP':
+      return { type: MOVE_UP };
+    case 'DOWN':
+      return { type: MOVE_DOWN };
+    case 'LEFT':
+      return { type: MOVE_LEFT };
+    case 'RIGHT':
+      return { type: MOVE_RIGHT };
+    default: {
+      return { type: IDLE };
     }
+  }
 }
 
-export function interact(text) {
-    return { type: INTERACT, text }
+export function interact() {
+  return { type: INTERACT };
 }
-
--- src/state/reducers.js
+++ src/state/reducers.js
@@ -1,78 +1,75 @@
 import { MOVE_UP, MOVE_DOWN, MOVE_LEFT, MOVE_RIGHT } from './actions';
 
-
 const initialGameState = {
-    character: {
-        position: {
-            x: 8,
-            y: 0,
-            f: 'N',
-            map: 'ROUTE_01'
-        }
+  character: {
+    position: {
+      x: 8,
+      y: 0,
+      f: 'N',
+      map: 'ROUTE_01'
     }
-
-}
+  }
+};
 
 function handleMoveUp(state) {
-    if (state.character.position.f !== 'N') {
-        let newState = Object.assign({}, state);
-        newState.character.position.f = 'N';
-        return newState;
-    } else {
-        let newState = Object.assign({}, state);
-        newState.character.position.y = state.character.position.y + 1;
-        return newState;
-    }
-};
+  if (state.character.position.f !== 'N') {
+    let newState = Object.assign({}, state);
+    newState.character.position.f = 'N';
+    return newState;
+  } else {
+    let newState = Object.assign({}, state);
+    newState.character.position.y = state.character.position.y + 1;
+    return newState;
+  }
+}
 
 function handleMoveDown(state) {
-    if (state.character.position.f !== 'S') {
-        let newState = Object.assign({}, state);
-        newState.character.position.f = 'S';
-        return newState;
-    } else {
-        let newState = Object.assign({}, state);
-        newState.character.position.y = state.character.position.y - 1;
-        return newState;
-    }
-};
+  if (state.character.position.f !== 'S') {
+    let newState = Object.assign({}, state);
+    newState.character.position.f = 'S';
+    return newState;
+  } else {
+    let newState = Object.assign({}, state);
+    newState.character.position.y = state.character.position.y - 1;
+    return newState;
+  }
+}
 
 function handleMoveLeft(state) {
-    if (state.character.position.f !== 'W') {
-        let newState = Object.assign({}, state);
-        newState.character.position.f = 'W';
-        return newState;
-    } else {
-        let newState = Object.assign({}, state);
-        newState.character.position.x = state.character.position.x + 1;
-        return newState;
-    }
-};
+  if (state.character.position.f !== 'W') {
+    let newState = Object.assign({}, state);
+    newState.character.position.f = 'W';
+    return newState;
+  } else {
+    let newState = Object.assign({}, state);
+    newState.character.position.x = state.character.position.x + 1;
+    return newState;
+  }
+}
 
 function handleMoveRight(state) {
-    if (state.character.position.f !== 'E') {
-        let newState = Object.assign({}, state);
-        newState.character.position.f = 'E';
-        return newState;
-    } else {
-        let newState = Object.assign({}, state);
-        newState.character.position.x = state.character.position.x - 1;
-        return newState;
-    }
-};
+  if (state.character.position.f !== 'E') {
+    let newState = Object.assign({}, state);
+    newState.character.position.f = 'E';
+    return newState;
+  } else {
+    let newState = Object.assign({}, state);
+    newState.character.position.x = state.character.position.x - 1;
+    return newState;
+  }
+}
 
 export function game(state = initialGameState, action) {
-
-    switch (action.type) {
-        case MOVE_UP:
-            return handleMoveUp(state);
-        case MOVE_DOWN:
-            return handleMoveDown(state);
-        case MOVE_LEFT:
-            return handleMoveLeft(state);
-        case MOVE_RIGHT:
-            return handleMoveRight(state);
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case MOVE_UP:
+      return handleMoveUp(state);
+    case MOVE_DOWN:
+      return handleMoveDown(state);
+    case MOVE_LEFT:
+      return handleMoveLeft(state);
+    case MOVE_RIGHT:
+      return handleMoveRight(state);
+    default:
+      return state;
+  }
 }
